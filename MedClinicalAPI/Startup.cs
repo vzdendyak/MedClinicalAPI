fe1a033e4f429347c286a5a70a9a67723c1e575c
Vasyl <vzdendyak@gmail.com>
2020-10-18 14:23:54 +0300

    Provided a connection string. Configured Startup.cs (Added swagger/identity/entity framework)

--- MedClinicalAPI/Startup.cs
+++ MedClinicalAPI/Startup.cs
@@ -1,15 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MedClinicalAPI.Data;
+using MedClinicalAPI.Data.Models;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
 
 namespace MedClinicalAPI
 {
@@ -25,12 +25,47 @@ namespace MedClinicalAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("LocalDb");
+            services.AddEntityFrameworkSqlServer().AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            services.AddIdentity<User, IdentityRole>(set =>
+            {
+                set.Password = new PasswordOptions()
+                {
+                    RequireNonAlphanumeric = false,
+                    RequireDigit = true,
+                    RequireLowercase = true,
+                    RequireUppercase = true,
+                    RequiredLength = 8
+                };
+                set.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppDbContext>();
+
             services.AddControllers();
+            var assembly = typeof(Startup).Assembly;
+            services.AddMediatR(assembly);
+            services.AddMvcCore().AddApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "MedClinicalAPI", Version = "v1" });
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                     {"Bearer", new string[0] }
+                };
+                options.CustomSchemaIds(f => f.FullName);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
