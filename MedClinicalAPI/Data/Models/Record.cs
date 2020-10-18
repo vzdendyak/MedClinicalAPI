0791549a342eb840a4a6a750ddf8f7d4622a87fe
Vasyl <vzdendyak@gmail.com>
2020-10-18 13:41:18 +0300

    Added an user model class. Changed record.cs -> userId is string and not the int

--- MedClinicalAPI/Data/Models/Record.cs
+++ MedClinicalAPI/Data/Models/Record.cs
@@ -5,8 +5,8 @@ namespace MedClinicalAPI.Data.Models
     public class Record
     {
         public int Id { get; set; }
-        public int DoctorId { get; set; }
-        public int PatientId { get; set; }
+        public string DoctorId { get; set; }
+        public string PatientId { get; set; }
         public DateTime DateOfRecord { get; set; }
         public DateTime DateOfMeeting { get; set; }
 
