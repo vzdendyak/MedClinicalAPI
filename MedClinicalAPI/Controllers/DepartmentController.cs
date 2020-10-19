70e969133a841d0403ea350fd58287802e10d4fc
Vasyl <vzdendyak@gmail.com>
2020-10-19 21:48:19 +0300

    Created a Department-controller. Created Get and GetById methods. Create 2 features (queries) for it.

---
+++ MedClinicalAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,55 @@
+﻿using MedClinicalAPI.Data.Models;
+using MedClinicalAPI.Features.Queries.DepartmentCRUD;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace MedClinicalAPI.Controllers
+{
+    [Route("api/departments")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DepartmentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var getQuery = new GetAllDepartments.Query();
+            var res = await _mediator.Send(getQuery);
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var getQuery = new GetDepartmentById.Query(id);
+            var res = await _mediator.Send(getQuery);
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync([FromBody] Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MedClinicalAPI/Features/Queries/DepartmentCRUD/GetDepartmentById/GetDepartmentById.cs
@@ -0,0 +1,61 @@
+﻿using MedClinicalAPI.Data;
+using MedClinicalAPI.Data.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MedClinicalAPI.Features.Queries.DepartmentCRUD
+{
+    public class GetDepartmentById
+    {
+        public class Query : IRequest<Department>
+        {
+            public int Id { get; set; }
+
+            public Query(int id)
+            {
+                Id = id;
+            }
+        }
+
+        public class Handler : IRequestHandler<GetDepartmentById.Query, Department>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Department> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var department = await _context.Departments.Where(d => d.Id == request.Id).Select(dep => new Department
+                {
+                    Id = dep.Id,
+                    DepartmentName = dep.DepartmentName,
+                    Address = new Address
+                    {
+                        Id = dep.Address.Id,
+                        City = dep.Address.City,
+                        HouseNumber = dep.Address.HouseNumber,
+                        Region = dep.Address.Region,
+                        Street = dep.Address.Street
+                    },
+                    AddressId = dep.AddressId,
+                    Description = dep.Description,
+                    Doctors = dep.Doctors.Select(doc => new User
+                    {
+                        Id = doc.Id,
+                        FirstName = doc.FirstName,
+                        LastName = doc.LastName,
+                        PhoneNumber = doc.PhoneNumber
+                    }).ToList(),
+                    ScheduleId = dep.ScheduleId
+                }).FirstOrDefaultAsync();
+                return department;
+            }
+        }
+    }
+}
