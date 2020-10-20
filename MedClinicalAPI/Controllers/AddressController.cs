691f3c7982459934a56626d21d7193444edaa2a3
ihyavorskyi <ihor201324@gmail.com>
2020-10-20 11:30:18 +0300

    Address, Service
    Created Address and Service controllers. Created GetAddressById, GetAllAddresses, GetServiceById and GetAllServices methods.

---
+++ MedClinicalAPI/Controllers/AddressController.cs
@@ -0,0 +1,57 @@
+﻿using MedClinicalAPI.Data.Models;
+using MedClinicalAPI.Features.Queries.AddressCRUD.GetAddressById;
+using MedClinicalAPI.Features.Queries.AddressCRUD.GetAllAddresses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+
+namespace MedClinicalAPI.Controllers
+{
+    [Route("api/addresses")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AddressController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var getQuery = new GetAllAddresses.Query();
+            var res = await _mediator.Send(getQuery);
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var getQuery = new GetAddressById.Query(id);
+            var res = await _mediator.Send(getQuery);
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync([FromBody] Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
