eeee0c9c5f125307155c565d5d2614764d9f79bc
Vasyl Zdendyak <vzdendyak@gmail.com>
2020-10-30 19:49:31 +0200

    Created Department and UserDto, provided including User and Services into GetDepById select query. (#15)

---
+++ WebAPI/MedClinicalAPI/Data/DTOs/DepartmentDto.cs
@@ -0,0 +1,24 @@
+﻿using MedClinicalAPI.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Data.DTOs
+{
+    public class DepartmentDto
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public string Description { get; set; }
+        public int AddressId { get; set; }
+        public int ScheduleId { get; set; }
+
+        // nav props
+        public Schedule Schedule { get; set; }
+
+        public Address Address { get; set; }
+        public ICollection<DepartmentService> DepartmentServices { get; set; }
+        public ICollection<UserDto> Doctors { get; set; }
+    }
+}
