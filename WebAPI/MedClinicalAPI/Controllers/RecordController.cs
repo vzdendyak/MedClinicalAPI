e39d0dda52b5a774b65f3b86e97c3e83ed1cfd49
Vasyl <vzdendyak@gmail.com>
2020-11-01 15:33:24 +0200

    added Create record functionality. validation: if the same record exist

---
+++ WebAPI/MedClinicalAPI/Controllers/RecordController.cs
@@ -0,0 +1,37 @@
+﻿using MedClinical.API.Features.Commands.RecordCRUD.CreateRecord;
+using MedClinicalAPI.Data.Models;
+using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RecordController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromBody] Record record)
+        {
+            var createRecordCommand = new CreateRecord.Command(record);
+            var res = await _mediator.Send(createRecordCommand);
+            return Ok(res);
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> GetAsync()
+        //{
+        //    var getQuery = new GetAllDepartments.Query();
+        //    var res = await _mediator.Send(getQuery);
+        //    return Ok(res);
+        //}
+    }
+}
---
+++ WebAPI/MedClinicalAPI/Features/Commands/RecordCRUD/CreateRecord/CreateRecord.cs
@@ -0,0 +1,41 @@
+﻿using MedClinicalAPI.Data;
+using MedClinicalAPI.Data.Models;
+using MedClinicalAPI.Helpers;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Features.Commands.RecordCRUD.CreateRecord
+{
+    public class CreateRecord
+    {
+        public class Command : IRequest<bool>
+        {
+            public Record Record { get; set; }
+
+            public Command(Record record)
+            {
+                Record = record;
+            }
+        }
+
+        public class Handler : IRequestHandler<CreateRecord.Command, bool>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
+            {
+                ValidationHelper.IsRecordExist(request.Record, _context);
+                await _context.Records.AddAsync(request.Record);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+        }
+    }
+}
