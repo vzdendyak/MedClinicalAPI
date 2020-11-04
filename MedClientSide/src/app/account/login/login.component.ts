b7eca9ab920cdf377f255d40b6aeffbbf7c65336
Vasyl <vzdendyak@gmail.com>
2020-11-04 21:30:01 +0200

    some style fixes. Changed header/footer.

--- MedClientSide/src/app/account/login/login.component.ts
+++ MedClientSide/src/app/account/login/login.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {LoginRequest} from '../../data/models/auth/login-request';
-import {DepartmentService} from '../../services/department.service';
 
 @Component({
   selector: 'app-login',
