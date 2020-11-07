bd8493a141eeb8b8af28c19b253b6a6a47f1c335
ihyavorskyi <ihor201324@gmail.com>
2020-11-07 17:59:25 +0200

    Created UserUpdateDto, UserController, update registration method

---
+++ WebAPI/MedClinicalAPI/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using MedClinical.API.Data.DTOs;
+using MedClinicalAPI.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Update(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            UserUpdateDto model = new UserUpdateDto
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                Age = user.Age
+            };
+            return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UserUpdateDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if (user != null)
+                {
+                    user.UserName = model.UserName;
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.Email = model.Email;
+                    user.PhoneNumber = model.PhoneNumber;
+                    user.Age = model.Age;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return Ok();
+        }
+    }
+}
--- WebAPI/MedClinicalAPI/Data/Models/Auth/RegisterRequest.cs
+++ WebAPI/MedClinicalAPI/Data/Models/Auth/RegisterRequest.cs
@@ -6,5 +6,7 @@
         public string Password { get; set; }
         public string ConfirmedPassword { get; set; }
         public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
