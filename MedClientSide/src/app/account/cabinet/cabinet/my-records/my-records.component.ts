6186fb132d123ab83d005c0065769b820edf56ab
Wizardzzzzz <dybrovskij2001@gmail.com>
2020-11-08 18:27:17 +0200

    cabinet

---
+++ MedClientSide/src/app/account/cabinet/cabinet/my-records/my-records.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-records',
+  templateUrl: './my-records.component.html',
+  styleUrls: ['./my-records.component.scss']
+})
+export class MyRecordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- MedClientSide/src/app/app.module.ts
+++ MedClientSide/src/app/app.module.ts
@@ -5,12 +5,12 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
+// import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from './material/material.module';
 import {HeaderComponent} from './navigation/header/header.component';
 import {FooterComponent} from './navigation/footer/footer.component';
-
+import {RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from './main-page/main-page.component';
 import {DepartmentComponent} from './department-functionality/department/department.component';
 import {DepartmentsListComponent} from './department-functionality/departments-list/departments-list.component';
@@ -19,12 +19,18 @@ import { CabinetComponent } from './account/cabinet/cabinet.component';
 import {LoginModule} from './auth/login.module';
 import {LoginComponent} from './auth/login/login.component';
 import {RegistrationComponent} from './auth/registration/registration.component';
+import { SettingsComponent } from './account/cabinet/cabinet/settings/settings.component';
+import { MyRecordsComponent } from './account/cabinet/cabinet/my-records/my-records.component';
+import { SupportComponent } from './account/cabinet/cabinet/support/support.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    CabinetComponent
+    CabinetComponent,
+    SettingsComponent,
+    MyRecordsComponent,
+    SupportComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,6 +42,9 @@ import {RegistrationComponent} from './auth/registration/registration.component'
         {path: 'auth/login', component: LoginComponent},
         {path: 'auth/registration', component: RegistrationComponent},
         {path: 'account/cabinet', component: CabinetComponent},
+        {path: 'account/cabinet/settings', component: SettingsComponent},
+        {path: 'account/cabinet/my-records', component: MyRecordsComponent},
+        {path: 'account/cabinet/support', component: SupportComponent},
         {path: 'departments', component: DepartmentsListComponent},
         {path: 'department/:id', component: DepartmentComponent},
         {path: 'main', component: MainPageComponent},
