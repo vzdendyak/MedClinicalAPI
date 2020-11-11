7e5e6d8fcd2d2e68c343167cde8a64e78b697e1e
Vasyl <vzdendyak@gmail.com>
2020-11-11 08:32:33 +0200

    Created doctor controller and doctor service to get free hours for doctor. Created featute GetDoctorHours. Created add-record form. Created doctor.service on client to send http request to server and get date

---
+++ MedClientSide/src/app/department-functionality/services/doctor.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {Department} from '../../data/models/department';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+  url: string = environment.apiUrl + '/doctors';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  getHours(id: string, date: number) {
+    return this.http.get(`${this.url}/${id}/${date}`);
+  }
+}
---
+++ WebAPI/MedClinicalAPI/Controllers/DoctorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MedClinical.API.Data.DTOs;
+using MedClinical.API.Features.Queries.GetHoursForDoctor;
+using MedClinical.API.Services.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedClinical.API.Controllers
+{
+    [Route("api/doctors")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+        private readonly IMediator _mediator;
+
+        public DoctorController(IDoctorService doctorService, IMediator mediator)
+        {
+            _doctorService = doctorService;
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}/{date}")]
+        public async Task<IActionResult> GetHoursForDoctor(string id, long date)
+        {
+            var command = new GetHoursForDoctor.Query(id, date);
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
+    }
+}
