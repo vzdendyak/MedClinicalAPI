2ac4dc4d6ee27abf5fbb9e7652a5ec52428d2341
Vasyl <vzdendyak@gmail.com>
2020-11-20 11:29:11 +0200

    added doctors-list

---
+++ MedClientSide/src/app/doctors/list-doctors/list-doctors.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-list-doctors',
+  templateUrl: './list-doctors.component.html',
+  styleUrls: ['./list-doctors.component.scss']
+})
+export class ListDoctorsComponent implements OnInit {
+
+  TitleMed(): void {
+    let doctorsBlock = document.getElementById('doctors_block');
+    let arrow = document.getElementById('arrow');
+    if (doctorsBlock.style.height === '0px' || doctorsBlock.style.height == 0) {
+      doctorsBlock.style.height = '500px';
+      arrow.style.transform = 'rotate(90deg)';
+    } else {
+      doctorsBlock.style.height = '0px';
+      arrow.style.transform = 'rotate(0deg)';
+    }
+    console.log(doctorsBlock);
+  }
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+}
