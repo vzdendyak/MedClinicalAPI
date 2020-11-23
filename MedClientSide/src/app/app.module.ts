dc04d01c127ef0b7cd6d240d1d5ce198953c3d56
Wizardzzzzz <dybrovskij2001@gmail.com>
2020-11-23 17:37:38 +0200

    кабінет

--- MedClientSide/src/app/app.module.ts
+++ MedClientSide/src/app/app.module.ts
@@ -29,6 +29,7 @@ import {MAT_DATE_LOCALE} from '@angular/material/core';
 import {CabinetNavComponent} from './navigation/cabinet-nav/cabinet-nav.component';
 import {AccountModule} from './account/account.module';
 import {ChangePasswordFormComponent} from './account/change-password-form/change-password-form.component';
+import { AboutComponent } from './About-us/about/about.component';
 
 export function tokenGetter() {
   return localStorage.getItem('jwt');
@@ -49,7 +50,8 @@ export const MY_DATE_FORMATS = {
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    AboutComponent
   ],
   entryComponents: [AddRecordFormComponent, ChangePasswordFormComponent],
   imports: [
@@ -68,6 +70,7 @@ export const MY_DATE_FORMATS = {
         {path: 'departments', component: DepartmentsListComponent},
         {path: 'department/:id', component: DepartmentComponent},
         {path: 'main', component: MainPageComponent},
+        {path: 'about', component: AboutComponent},
         {path: '**', redirectTo: 'main'}
 
       ]),
