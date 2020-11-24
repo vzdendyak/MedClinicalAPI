ba27bd86036e29030c1a6ddf77ab8534e8393a31
ihyavorskyi <ihor201324@gmail.com>
2020-11-24 19:08:57 +0200

    created scheduleController. Created GetAllSchedules and DeleteSchedule methods

---
+++ WebAPI/MedClinicalAPI/Controllers/ScheduleController.cs
@@ -0,0 +1,36 @@
+﻿using MedClinical.API.Features.Commands.ScheduleCRUD.DeleteSchedule;
+using MedClinical.API.Features.Queries.ScheduleCRUD.GetAllSchedule;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Controllers
+{
+    [Route("api/schedules")]
+    [ApiController]
+    public class ScheduleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ScheduleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var getQuery = new GetAllSchedule.Query();
+            var res = await _mediator.Send(getQuery);
+            return Ok(res);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var command = new DeleteSchedule.Command(id);
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
+    }
+}
---
+++ WebAPI/MedClinicalAPI/Features/Commands/ScheduleCRUD/DeleteSchedule/DeleteSchedule.cs
@@ -0,0 +1,45 @@
+﻿using MedClinicalAPI.Data;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Features.Commands.ScheduleCRUD.DeleteSchedule
+{
+    public class DeleteSchedule
+    {
+        public class Command : IRequest<bool>
+        {
+            public int ScheduleId { get; set; }
+
+            public Command(int scheduleId)
+            {
+                ScheduleId = scheduleId;
+            }
+        }
+
+        public class Handler : IRequestHandler<DeleteSchedule.Command, bool>
+        {
+            private readonly AppDbContext _context;
+
+            public Handler(AppDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<bool> Handle(Command command, CancellationToken cancellationToken)
+            {
+                var result = await _context.Schedules.FindAsync(command.ScheduleId);
+                if (result != null)
+                {
+                    _context.Schedules.Remove(result);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
