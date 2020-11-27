d26ff731137fd47b5708a3cd20aa97b1467ce445
Vasyl <vzdendyak@gmail.com>
2020-11-27 17:30:46 +0200

    Create service at admin panel

---
+++ MedClientSide/src/app/account/forms/create-service-form/create-service-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-service-form',
+  templateUrl: './create-service-form.component.html',
+  styleUrls: ['./create-service-form.component.scss']
+})
+export class CreateServiceFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
