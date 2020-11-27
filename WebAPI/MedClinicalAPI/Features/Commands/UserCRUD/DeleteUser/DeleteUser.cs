35343c551f40559f1e1cc9cb2a1038030fbe7a72
ihyavorskyi <ihor201324@gmail.com>
2020-11-27 12:43:06 +0200

    delete user

---
+++ WebAPI/MedClinicalAPI/Features/Commands/UserCRUD/DeleteUser/DeleteUser.cs
@@ -0,0 +1,46 @@
+﻿using MedClinicalAPI.Data;
+using MedClinicalAPI.Data.Models;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MedClinical.API.Features.Commands.UserCRUD.DeleteUser
+{
+    public class DeleteUser
+    {
+        public class Command : IRequest<bool>
+        {
+            public string UserId { get; set; }
+
+            public Command(string userId)
+            {
+                UserId = userId;
+            }
+        }
+
+        public class Handler : IRequestHandler<DeleteUser.Command, bool>
+        {
+            private readonly AppDbContext _context;
+            private UserManager<User> _userManager;
+
+            public Handler(AppDbContext context, UserManager<User> userManager)
+            {
+                _context = context;
+                _userManager = userManager;
+            }
+
+            public async Task<bool> Handle(Command command, CancellationToken cancellationToken)
+            {
+                var user = await _userManager.FindByIdAsync(command.UserId);
+                if (user != null)
+                {
+                    await _userManager.DeleteAsync(user);
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
